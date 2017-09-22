7cf4ea9b8f8b9fd046d3743ea2dd8a153f765bb2
LAPTOP-RICHI\Richi <richard.dangl@hakmistelbach.ac.at>
2017-09-22 09:11:42 +0200

    Button hinzugefügt "Click me"

--- MyFirstRepo/Form1.Designer.cs
+++ MyFirstRepo/Form1.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(80, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me!!!";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(454, 337);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "MyFirstRepo";
             this.ResumeLayout(false);
@@ -42,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
